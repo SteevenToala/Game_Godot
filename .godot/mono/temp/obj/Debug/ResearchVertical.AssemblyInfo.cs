72b858bc83da100cb2d162c88b7036d19986eef1
SteevenToala <steevenpfb@gmail.com>
2025-12-04 07:27:54 -0500

    feature: mejora de texturas

--- .godot/mono/temp/obj/Debug/ResearchVertical.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/ResearchVertical.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResearchVertical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7548aca6f98504763531dc813bf8e524f440706")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResearchVertical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResearchVertical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
